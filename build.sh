9ebe2ffa551526345a210c4dd44d462274ecb7e4
ThoNohT <e.c.p.bataille@gmail.com>
2024-03-24 13:45:27 +0100

    Some restructuring and cleanup
    I don't need to compile raylib here, so just include the library and
    header files. Move them to respective lib and include folders. Also move
    build output to the build folder and add a clean task.

--- build.sh
+++ build.sh
@@ -3,14 +3,19 @@
 set -xe
 
 SRC="./src/main.c"
-CFLAGS="-Wall -Wextra -ggdb -I./raylib-5.0/src"
-LIBS="-lm -ldl -lrt -L. -l:libraylib.a"
+CFLAGS="-Wall -Wextra -ggdb -I./include/raylib-5.0"
+LIBS="-lm -L./lib -l:libraylib.a"
 
 if [ $1 = "build" ]; then
-    clang $CFLAGS -Wall -o ./main $SRC $LIBS
+    mkdir -p ./build
+    clang $CFLAGS -Wall -o ./build/main $SRC $LIBS
 
 elif [ $1 = "run" ]; then
-    clang $CFLAGS -Wall -o ./main $SRC $LIBS && ./main
+    mkdir -p ./build
+    clang $CFLAGS -Wall -o ./build/main $SRC $LIBS && ./build/main
+
+elif [ $1 = "clean" ]; then
+    rm -rf ./build
 
 else
     echo "Invalid command."
